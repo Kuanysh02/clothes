84111732429653c1e3ccabb6e7c2df8ad17b3c80
Kuanysh02 <kuanyshyerkin02@gmail.com>
2022-05-17 14:39:07 +0600

    router render shop ejs temp

--- routes/shop.js
+++ routes/shop.js
@@ -5,4 +5,9 @@ const router = express.Router();
 router.get('/', controller.findAll);
 router.post('/', controller.create);
 
+
+router
+    .route("/")
+    .get((req, res) => res.render("shop.ejs"))
+
 module.exports = router;
